dfeba2261aa3dca2865141200d4dd47243a3fe51
DEMENTOR <dmntr@rambler.ru>
2018-12-09 01:42:50 +1000

    fullpage

--- functions.php
+++ functions.php
@@ -122,6 +122,17 @@ add_action( 'widgets_init', 'jppv_widgets_init' );
 function jppv_scripts() {
 	wp_enqueue_style( 'jppv-style', get_stylesheet_uri() );
 
+	//jquery.min.js
+	wp_enqueue_script( 'jquery-script', get_template_directory_uri() . '/js/jquery.min.js', array(), ' ' );
+	//bootstrap
+	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css', array(), ' ' );
+	wp_enqueue_script( 'bootstrap-script', get_template_directory_uri() . '/js/bootstrap.min.js', array(), ' ' );
+	//fullpage
+	wp_enqueue_style( 'fullpage-style', get_template_directory_uri() . '/css/fullpage.css', array(), ' ' );
+	
+	// wp_enqueue_script( 'fullpage-script', get_template_directory_uri() . '/js/fullpage.js', array(), ' ' );
+	// wp_enqueue_script( 'fullpage-script-setup', get_template_directory_uri() . '/js/index.js', array(), ' ' );
+	
 	wp_enqueue_script( 'jppv-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'jppv-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
---
+++ js/index.js
@@ -0,0 +1,14 @@
+new fullpage('#fullpage', {
+	licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+	anchors: ['firstPage', 'secondPage', 'thirdPage'],
+	scrollBar: false,
+	// navigation:true,
+	navigationTooltips: ['01','02','03'],
+	showActiveTooltip: true,
+	slidesNavigation: true,
+	navigationPosition: 'left',
+	slidesNavPosition: 'top',
+	loopHorizontal: false,
+	//visual
+	//fixedElements: '#header, #footer'
+});
