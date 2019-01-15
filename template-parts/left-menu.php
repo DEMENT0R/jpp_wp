28f56abb4cc47987b52a1fdcd5238c9daf77d79e
DEMENT0R <dmntr@rambler.ru>
2019-01-15 15:13:47 +1000

    Update left-menu.php

--- template-parts/left-menu.php
+++ template-parts/left-menu.php
@@ -1,11 +1,13 @@
-	<?php if (is_page(32)) {
+<?php 
+	if (is_page(32)) {
 		include ('left-menu-sections.php');
-	} ?>
+	}
 
-	<?php if (is_page(35)) {
+	if (is_page(35)) {
 		include ('left-menu-investoru.php');
-	} ?>
+	}
 
-	<?php if (is_category(1)) {
+	if (is_category(1)) {
 		include ('left-menu-press-centr.php');
-	} ?>
+	}
+?>
