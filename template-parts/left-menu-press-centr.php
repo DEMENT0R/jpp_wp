a2c728feeef77d3708a122065a35272e38ef503c
DEMENTOR <dmntr@rambler.ru>
2019-01-20 22:10:19 +1000

    press centr

--- template-parts/left-menu-press-centr.php
+++ template-parts/left-menu-press-centr.php
@@ -1,3 +1,30 @@
 	<div class="left-menu-press-centr">
-		<h1 class="mt-5 pt-5">TEST block for left-menu-press-centr</h1>
+		<style>
+			.left-menu-press-centr ul {
+				margin:  0;
+				padding: 0;
+				text-decoration: none;
+			    list-style-image: none;
+			    list-style-type: none;
+			}
+		</style>
+		<div class="row col-12" style="margin-top: 176px;">
+			<div class="col-6 offset-4">Пресс-центр/Пресс-релизы</div>
+			<div class="col-2 text-right menu-item"><a href="#">
+				<svg style="margin-right: 12px;" width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M0 6.5L11.25 0.00480938V12.9952L0 6.5Z" fill="black"/>
+				</svg>
+			Назад</a></div>
+		</div>
+		<div class="row col-12" style="margin-top: 470px;">
+			<div class="col-6 offset-4">
+				<ul class="menu-item">
+					<li><a href="#">Платформа</a></li>
+					<li><a href="#">Цели и задачи</a></li>
+					<li><a href="#">Направление деятельности</a></li>
+					<li><a href="#">Руководство</a></li>
+					<li><a href="#">JBIK Bank</a></li>
+				</ul>
+			</div>
+		</div>
 	</div>
